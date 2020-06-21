b8f5f5ba69656686593748593d4aca012cb1c1de
Gustavo Freitas Ferreira <gufreitasf@gmail.com>
2020-06-21 18:57:11 -0300

    Correção do cálculo do número de dispositivos atualizados, desatualizados e em manutenção

--- src/DashboardPage.js
+++ src/DashboardPage.js
@@ -57,15 +57,17 @@ class DashboardPage extends Component {
         } else {
             const divStyle = {float: "left", "margin": "20px" };
             const devices = this.props.devices;
+
+            let loadTime = new Date();
             const updated = this.devicesBetweenTime( devices,
-                                                    new Date(this.state.loadTime.getTime() - 6*3600000), //Última 6 horas 
-                                                    this.state.loadTime );
+                                                    new Date(loadTime.getTime() - 6*3600000), //Última 6 horas 
+                                                    loadTime );
             const outdated = this.devicesBetweenTime( devices,
-                                                    new Date(this.state.loadTime.getTime() - 72*3600000), //Entre 6 e 72h
-                                                    new Date(this.state.loadTime.getTime() - 6*3600000) );
+                                                    new Date(loadTime.getTime() - 72*3600000), //Entre 6 e 72h
+                                                    new Date(loadTime.getTime() - 6*3600000) );
             const maintenance = this.devicesBetweenTime( devices,
                                                     new Date(0), //Maior que 72h
-                                                    new Date(this.state.loadTime.getTime() - 72*3600000) );
+                                                    new Date(loadTime.getTime() - 72*3600000) );
             return (
                 <Layout history={this.props.history}>
                     <h1 align="center">Dashboard dos dispositivos</h1>
