555c776bbd73f3ba533cfd47b9cbc387eb8a1bd9
Gustavo Freitas Ferreira <gufreitasf@gmail.com>
2020-06-21 18:49:31 -0300

    Altera o fundo da imagem do dispositivo de acordo com a data e hora de atualizacao

--- src/DeviceImage.js
+++ src/DeviceImage.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
-import SVG from 'react-inlinesvg';
+import { SvgLoader, SvgProxy } from 'react-svgmt';
 
 export default class DeviceImage extends Component {
     
     render() {
         let category = this.props.category;
+        let loadTime = new Date();
+        let lastUpdate = new Date( Date.parse( this.props.lastUpdate !==null ? this.props.lastUpdate : "01/01/2000") );
+        let last6hours = new Date( loadTime.getTime() - 6*3600000 ); //Última 6 horas
+        let last72hours = new Date( loadTime.getTime() - 72*3600000 ); //Última 72 horas
+
         if(category == null)
             category = "car";
+
+        let fill = "#009933";
+        if(lastUpdate > last6hours) {
+            fill = "#009933";
+        }
+        else if(lastUpdate > last72hours && lastUpdate <= last6hours) {
+            fill = "#ff9933";
+        }
+        else {
+            fill = "#ff3300";
+        }
+
         const url = "images/" + category + ".svg";
-        const Icon = () => <SVG src={url} />;
-        // return <img src={"categoty/" + category + ".svg"} alt=""/>
-        return <Icon />
+        return (
+        <SvgLoader path={url}>
+            <SvgProxy selector="#background" fill={fill}/>
+        </SvgLoader>
+        )
     }
 };
