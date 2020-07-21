ce32af0e1d448294eb9f5bb5c20f0a8d036c6ae3
Gustavo Freitas Ferreira <gufreitasf@gmail.com>
2020-07-21 19:32:22 -0300

    Correção do DeviceSelector para permitir inserir o valor inicial

--- src/DeviceSelector.js
+++ src/DeviceSelector.js
@@ -5,39 +5,63 @@ import TextField from '@material-ui/core/TextField';
 
 const mapStateToProps = state => ({
     devices: state.devices
-  });
+});
 
 class DeviceSelector extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedDevices: []
+        }
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.defaultValue !== prevProps.defaultValue) {
+            const defaultValue = this.props.defaultValue;
+            if (defaultValue != null) {
+                this.setState({
+                    selectedDevices: defaultValue
+                });
+            }
+            else {
+                this.setState({
+                    selectedDevices: []
+                });
+            }
+        }
+    }
+
     render() {
-        let { devices, defaultValue, disableClearable } = this.props;
-        if(defaultValue === null)
-            defaultValue = [];
+        let { devices, disableClearable } = this.props;
+        let { selectedDevices } = this.state;
 
-        if( devices && devices.length > 0 ) {
+        if (devices && devices.length > 0) {
 
-            return  (
+            return (
                 <Autocomplete
                     multiple
                     id="tags-standard"
                     options={devices}
-                    defaultValue={defaultValue}
+                    value={selectedDevices}
                     disableClearable={disableClearable}
                     clearText="Limpar seleção"
                     filterSelectedOptions={false}
                     style={{ maxWidth: 400 }}
                     getOptionLabel={(option) => option.name}
+                    getOptionSelected={(option) => option.name}
                     renderInput={(params) => (
-                    <TextField
-                        {...params}
-                        variant="standard"
-                        label="Nome do dispositivo"
-                        placeholder="Dispositivo"
-                        margin="normal"
-                    />
+                        <TextField
+                            {...params}
+                            variant="standard"
+                            label="Nome do dispositivo"
+                            placeholder="Dispositivo"
+                            margin="normal"
+                        />
                     )}
                     onChange={(event, newValue) => {
                         this.props.onChange(event, newValue);
-                      }}
+                    }}
                 />
             );
         }
